3a2b8ba26365252788bd4d8ea12bc611452ebf0e
YunisAslan <yunisaslanov397@gmail.com>
2023-08-16 19:10:20 +0400

    feat: implemented subscribe & unsubscribe to subreddit for each users

---
+++ src/app/api/subreddit/unsubscribe/route.ts
@@ -0,0 +1,66 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+import { SubredditSubscriptionValidator } from "@/lib/validators/subreddit";
+import { db } from "@/lib/db";
+import { z } from "zod";
+
+export async function POST(request: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await request.json();
+
+    const { subredditId } = SubredditSubscriptionValidator.parse(body);
+
+    // check if user has already subscribed to subreddit
+    const subscriptionExists = await db.subscription.findFirst({
+      where: {
+        subredditId,
+        userId: session?.user.id,
+      },
+    });
+
+    if (!subscriptionExists) {
+      return new Response("You are not subscribed to this subreddit.", {
+        status: 400,
+      });
+    }
+
+    // check if user is the creator of the subreddit
+    const subreddit = await db.subreddit.findFirst({
+      where: {
+        id: subredditId,
+        creatorId: session.user.id,
+      },
+    });
+
+    if (subreddit) {
+      return new Response("You can't unsubscribe from your own subreddit", {
+        status: 400,
+      });
+    }
+
+    await db.subscription.delete({
+      where: {
+        userId_subredditId: {
+          subredditId,
+          userId: session.user.id,
+        },
+      },
+    });
+
+    return new Response(subredditId);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+
+    return new Response("Could not unsubscribe, please try again later.", {
+      status: 500,
+    });
+  }
+}
--- src/components/SubscribeLeaveToggle.tsx
+++ src/components/SubscribeLeaveToggle.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/Button";
 import { SubscribeToSubredditPayload } from "@/lib/validators/subreddit";
 import axios, { AxiosError } from "axios";
 import UseCustomToast from "@/hooks/use-custom-toast";
-import { toast, useToast } from "@/hooks/use-toast";
+import { useToast } from "@/hooks/use-toast";
 import { startTransition } from "react";
 import { useRouter } from "next/navigation";
 
@@ -22,8 +22,8 @@ function SubscribeLeaveToggle({
 }: SubscribeLeaveToggleProps) {
   // const isSubscribed = false; //mock
   const { toast } = useToast();
-  const router = useRouter();
   const { loginToast } = UseCustomToast();
+  const router = useRouter();
 
   const { mutate: subscribe, isLoading: isSubLoading } = useMutation({
     mutationFn: async () => {
@@ -55,15 +55,58 @@ function SubscribeLeaveToggle({
         router.refresh();
       });
 
-      toast({
+      return toast({
         title: "Subscribed",
         description: `You are now subscribed to t/${subredditName}`,
       });
     },
   });
 
+  const { mutate: unsubscribe, isLoading: isUnSubLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToSubredditPayload = {
+        subredditId,
+      };
+
+      const { data } = await axios.post("/api/subreddit/unsubscribe", payload);
+
+      return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "There was a problem",
+        description: "Something went wrong, please try again.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        // Refresh the current route and fetch new data from the server without
+        // losing client-side browser or React state.
+        router.refresh();
+      });
+
+      return toast({
+        title: "Unsubscribed",
+        description: `You are now unsubscribed from t/${subredditName}`,
+      });
+    },
+  });
+
   return isSubscribed ? (
-    <Button className="w-full mt-1 mb-4">Leave community</Button>
+    <Button
+      isLoading={isUnSubLoading}
+      onClick={() => unsubscribe()}
+      className="w-full mt-1 mb-4"
+    >
+      Leave community
+    </Button>
   ) : (
     <Button
       isLoading={isSubLoading}
